bffcca0eff34eb4c8a5552665e93db17adc85966
Gang Tao <gang@timeplus.io>
2026-02-23 18:25:36 -0800

    add umd build

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,17 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 import dts from 'rollup-plugin-dts';
+import terser from '@rollup/plugin-terser';
 
 const external = ['react', 'react-dom', '@antv/g2', '@antv/s2', 'lodash', 'ramda'];
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  '@antv/g2': 'G2',
+  '@antv/s2': 'S2',
+  lodash: '_',
+  ramda: 'R',
+};
 
 export default [
   // Main bundle
@@ -20,6 +29,21 @@ export default [
         file: 'dist/index.esm.js',
         format: 'esm',
         sourcemap: true,
+      },
+      {
+        file: 'dist/index.umd.js',
+        format: 'umd',
+        name: 'Vistral',
+        globals,
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.umd.min.js',
+        format: 'umd',
+        name: 'Vistral',
+        globals,
+        sourcemap: true,
+        plugins: [terser()],
       },
     ],
     external,
